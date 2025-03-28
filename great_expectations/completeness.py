e8754b0991f81401d8eba4ed926a989977505cd8
andy <has32photo@gmail.com>
2025-03-28 14:07:18 +0300

    Update completeness.py
    Signed-off-by: andy <has32photo@gmail.com>

--- great_expectations/completeness.py
+++ great_expectations/completeness.py
@@ -19,11 +19,12 @@ df_ge.head()
 # воспользуемся методом "expect_column_values_to_not_be_null"
 
 check1_Cabin = df_ge.expect_column_values_to_not_be_null(   # "значение колонки не Null"
-    column = 'Cabin', # название исследуемой колонки
-    mostly = 0.25   # пороговое значение, говорящее, что "как минимум 25% должно быть не Null"
+    column = 'Cabin',  # название исследуемой колонки
+    mostly = 0.25      # пороговое значение, говорящее, что "как минимум 25% должно быть не Null"
 )
 
-print('check1_Cabin: ', check1_Cabin)
+print('check1_Cabin: ', check1_Cabin) # так мы увидим полный результат нашей проверки
+# "success": true/false — проверка пройдена/не пройдена
 
 # В result:
 # — "element_count" это размер нашего датафрейма,
@@ -32,19 +33,19 @@ print('check1_Cabin: ', check1_Cabin)
 # — "partial_unexpected_list" это список значений, которые не удовлетворяют условимям проверки,
 # NaN/Null туда не пишется, ибо это "пустота"
 
-if not check1_Cabin['success']:
-    print(check1_Cabin['result'])
+if not check1_Cabin['success']:     # удобная конструкция, если проверка не пройдена, то мы увидим
+    print(check1_Cabin['result'])   # результаты проверки без лишней информации
 
 
-# на примере колонки "Ticket"
+# Повторим на примере другой колонки — "Ticket"
 check1_Ticket = df_ge.expect_column_values_to_not_be_null(
     column = 'Ticket',
     mostly = 0.99
 )
 
-print('check1_Ticket: ', check1_Ticket)
+print('check1_Ticket: ', check1_Ticket) # так мы увидим полный результат нашей проверки
 
-if not check1_Ticket['success']:
-    print(check1_Ticket['result'])
+if not check1_Ticket['success']:     # если проверка не пройдена, то мы увидим результаты
+    print(check1_Ticket['result'])   # проверки без лишней информации
 
-# никакого вывода, т.к. абсолютно для всех строк присутствует значение в колонке "Ticket"
+# Здесь мы не увидим никакого вывода, т.к. абсолютно для всех строк присутствует значение в колонке "Ticket".
