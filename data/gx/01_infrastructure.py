a3802792209ab960072d3e83b580430a4639c63e
andy <has32photo@gmail.com>
2025-07-20 17:04:00 +0300

    Update file

--- data/gx/01_infrastructure.py
+++ data/gx/01_infrastructure.py
@@ -6,36 +6,7 @@ df.head()
 
 print(df.dtypes)
 
-# PassengerId      int64
-# Survived         int64
-# Pclass           int64
-# Name            object
-# Sex             object
-# Age            float64
-# SibSp            int64
-# Parch            int64
-# Ticket          object
-# Fare           float64
-# Cabin           object
-# Embarked        object
-# dtype: object
-
 print(df['Cabin'])
-
-# 0       NaN
-# 1       C85
-# 2       NaN
-# 3      C123
-# 4       NaN
-#        ...
-# 886     NaN
-# 887     B42
-# 888     NaN
-# 889    C148
-# 890     NaN
-# Name: Cabin, Length: 891, dtype: object
-
 
 df_ge = gx.from_pandas(df)
 df_ge.head()
-
